94c9f1dc3ccfe758ce009c178054f4bcf992b1d9
Timka139 <bond139liza@mail.ru>
2016-12-11 15:49:21 +0300

    Оптимизация меню

--- js/script.js
+++ js/script.js
@@ -4,18 +4,16 @@
 //     if (err) throw err;
 // });
 // console.log(fs.readFileSync("test.txt",{encoding: 'utf-8'}));
-var i;
-i=2;
 function menuBtn() {
-     var myMenu=$(".my-nav-bar li"), style = myMenu[0].style;
-     if (i%2==0) {
+     var myMenu=$(".my-nav-li"), style = myMenu[0].style;
+     console.log(myMenu);
+     if (style.height == "160px") {
+        style.transition = "all .8s ease-in-out";
+     style.height = "0px";
+     }  
+     else {
          style.transition = "all .8s ease-in-out";
      style.height = "160px";
      style.width = "100%";
-     } else {
-         style.transition = "all .8s ease-in-out";
-     style.height = "0px";
-     style.width = "0%";
-     };
-     i=i++;
+     } 
 }
